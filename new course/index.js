846c74ff8f788e1d3d523cc37fd5192757b66734
ninishka <zambakhidzenini.740@gmail.com>
2024-05-29 12:26:21 +0400

    old js practice of first one month, found just now and save it on git

---
+++ new course/index.js
@@ -0,0 +1,12 @@
+let age = 21;//numbers
+let firstname = "bro";//strings
+let student = true; //booleans
+
+console.log("hello", firstname); 
+console.log("you are", age, "years old");
+console.log("enrolled:", student);
+
+
+document.getElementById(p1).innerHTML = "hello " + firstname;
+document.getElementById(p2).innerHTML = "you are " + age + " years old";
+document.getElementById(p3).innerHTML = "enrolled: " +  student;
